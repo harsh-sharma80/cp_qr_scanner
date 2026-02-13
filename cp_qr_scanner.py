9c9b2c828fd2aa59f6283d982efad8933b5dbf45
Harsh Sharma <harsh426711@gmail.com>
2026-02-13 16:03:16 +0530

    Scan json, Store json

--- cp_qr_scanner.py
+++ cp_qr_scanner.py
@@ -1,9 +1,8 @@
-#testing on pc
 from kivy.config import Config
-Config.set('graphics', 'width', '360')
-Config.set('graphics', 'height', '640')
 
-
+# Set default window size
+Config.set('graphics', 'width', '675')
+Config.set('graphics', 'height', '1500')
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
@@ -11,6 +10,8 @@ from kivy.uix.label import Label
 from kivy.uix.camera import Camera
 from kivy.uix.button import Button
 from kivy.uix.image import Image as KivyImage
+
+from kivy.graphics import Color, Rectangle
 
 from kivy.clock import Clock
 from kivy.core.window import Window
@@ -20,7 +21,7 @@ from PIL import Image
 import requests
 from datetime import datetime
 import textwrap
-
+import json
 
 
 # your deployed Google Apps Script URL
@@ -32,53 +33,74 @@ class QRScanner(FloatLayout):
         super().__init__(**kwargs)
 
         self.orientation = "vertical"
+        with self.canvas.before:
+            Color(0.267, 0.267, 0.267, 1.0)  
+            self.bg_rect = Rectangle(pos=self.pos, size=self.size)
+
+        self.bind(size=self.update_bg, pos=self.update_bg)
 
         # Camera widget (Android-safe)
         self.camera = Camera(
             resolution=(800, 600),
             play=True,
-            size_hint=(1,1),
-            pos_hint={'x':0, 'y':0.16}
+            size_hint=(1,0.5),
+            pos_hint={'x':0, 'y':0.3}
         )
         self.add_widget(self.camera)
 
         # Logo
-        self.corner_img = KivyImage(source="clublogo.png",  
-                                size_hint=(None, None),
-                                size = (80,80),
-                                pos_hint={'x':0.0, 'top':1})  # top-left corner
-        self.add_widget(self.corner_img)
+        self.logo = KivyImage(source="clublogo.png",  size_hint=(0.2,0.2), pos_hint={'x':0.0, 'top':1})  # top-left corner
+        self.add_widget(self.logo)
 
-        # Status labela
+        # Label
         self.label = Label(
             text="Ready to Scan",
+            color = (1,1,1,1),    #white
             size_hint=(1, 0.2),
             pos_hint={'x':0,'y':0.1},
             font_size=22
         )
         self.add_widget(self.label)
 
-        # Start scanning button
-        self.start_btn = Button(text="Start Scanning", size_hint=(1, 0.1))
+        # Start button
+        self.start_btn = Button(
+            text="Start Scanning",
+            color = (1,1,1,1),    #white
+            size_hint=(0.5, 0.1),
+            pos_hint={"x":0.25,"y":0},
+            background_normal='',
+            background_color=(0, 0.5, 1, 1))
+        
         self.start_btn.bind(on_press=self.start_scanning)
         self.add_widget(self.start_btn)
 
         self.last_qr = None
         self.scanning_event = None  # Store Clock event so we can start/stop
 
+    def update_bg(self, *args):
+        self.bg_rect.pos = self.pos
+        self.bg_rect.size = self.size
 
+    #expected qr format -> {"Key1":"value1","Key2":"value2"}
+    def is_valid_json_qr(self, data):
+        required_fields = ["Name", "Roll", "Email"] 
+        return isinstance(data, dict) and all(field in data for field in required_fields) 
+    
     def start_scanning(self, instance):
         if self.scanning_event:
             # Stop scanning
             Clock.unschedule(self.scanning_event)
             self.scanning_event = None
             self.label.text = "Scanning stopped"
-            self.start_btn.text = "Scan"
+            color = (1,1,1,1)    #white
+            self.label.color = (1,1,1,1)    #white
+            self.start_btn.text = "Scan Again"
         else:
             # Start scanning
             self.label.text = "Point at QR code"
+            self.label.color = (1,1,1,1)    #white
             self.scanning_event = Clock.schedule_interval(self.scan_qr, 0.5)
-            self.start_btn.text = "Stop Scanning"    
+            self.start_btn.text = "Stop Scanning"   
 
     def scan_qr(self, dt):
         texture = self.camera.texture
@@ -98,17 +120,29 @@ class QRScanner(FloatLayout):
 
         qr_text = decoded[0].data.decode("utf-8")
 
-        # Prevent duplicate scans
-        if qr_text == self.last_qr:
-            self.label.text = f"Same as last saved QR"
+        try:
+            qr_data = json.loads(qr_text)
+        except json.JSONDecodeError:
+            self.label.text = "Invalid QR format"
+            self.label.color = (1, 0, 0, 1)
             return
 
-        self.last_qr = qr_text
+        # Validate required fields
+        if not self.is_valid_json_qr(qr_data):
+            self.label.text = "QR missing values"
+            self.label.color = (1, 0, 0, 1)
+            return
+        
+        # Prevent duplicate scans
         timestamp = datetime.now().isoformat()
 
-        self.label.text = f"Saving...\n{qr_text}"
+        # Check for today's duplicate entry
+        if self.already_scanned_today(qr_data):
+            self.label.text = "Duplicate Entry"
+            self.label.color = (1, 0, 0, 1)  # red
+            return
 
-        self.send_to_sheet(qr_text, timestamp)
+        self.send_to_file(qr_data, timestamp)
 
         # Pause scanning briefly
         Clock.unschedule(self.scan_qr)
@@ -116,22 +150,44 @@ class QRScanner(FloatLayout):
 
     def resume_scanning(self, dt):
         self.label.text = "Point camera at QR code"
+        self.label.color = (1,1,1,1)    #white
         if self.scanning_event:
             self.scanning_event = Clock.schedule_interval(self.scan_qr, 0.5)
 
+    def already_scanned_today(self, qr_data):
+        today = datetime.now().date().isoformat()
+        roll = qr_data.get("Roll")
 
+        try:
+            with open("qr_output.jsonl", "r") as f:
+                for line in f:
+                    saved = json.loads(line)
+                    saved_date = saved.get("timestamp", "").split("T")[0]
+                    saved_roll = saved.get("Roll")
+
+                    if saved_date == today and saved_roll == roll:
+                        return True
+        except FileNotFoundError:
+            return False
+
+        return False
 
 
     #Data sharing to google sheet
-    def send_to_sheet(self, qr_text, timestamp):
+    def send_to_file(self, qr_data, timestamp):
         try:
-            with open("qr_output.txt", "a") as f:
-                f.write(f"{timestamp} - {qr_text}\n")
-            wrapped_text= "\n".join(textwrap.wrap(qr_text, width=20))
-            self.label.text = f"Saved locally\n{wrapped_text}"
+            qr_data["timestamp"] = timestamp  # add timestamp
+
+            with open("qr_output.jsonl", "a") as f:
+                f.write(json.dumps(qr_data) + "\n")
+
+            self.label.text = f"Saved:\n{qr_data.get('Name')}"
+            self.label.color = (0, 1, 0, 1)
+
         except Exception as e:
-            self.label.text = f"File error"
-            print("Error writing QR:", e)
+            self.label.text = "File Error"
+            self.label.color = (1, 0, 0, 1)
+            print("Error:", e)
 
 
 class QRApp(App):
